88548e5f3937e3f264b0bf1ea2b9211346bebfc6
patrickatcw <pj8392@yahoo.com>
2017-10-27 11:46:33 -0400

    progressing..path maybe wrong

--- controllers/burgers_controller.js
+++ controllers/burgers_controller.js
@@ -4,25 +4,20 @@ var methodOverride = require("method-override");
 
 var router = express.Router();
 
-// Import the model (cat.js) to use its database functions.
-var db = require("../models/burgers.js");
+var db = require("../models/");
 
-// Create all our routes and set up logic within those routes where required.
-router.get("/", function(reg,res) {
-  db.Burger.findAll(function(data) {
-    var retObject = {
-      burgers: data
-    };
-      res.render("index", retObject);
+
+router.get("/api/burgers", function(req, res) {
+    db.Burger.findAll({}).then(function(data) {
+    res.render("index", data);
     });
-});
+  });
 
 router.post("/api/burgers", function(req, res) {
   burger.Todo.create({
       text: req.body.text,
       complete: req.body.complete
     }).then(function(burgerTodo) {
-      // We have access to the new todo as an argument inside of the callback function
       res.json(burgerTodo)
     });
 });
@@ -36,7 +31,6 @@ router.put("/api/burgers/:id", function(req, res) {
     devoured: true
   }, condition, function(result) {
     if (result.changedRows == 0) {
-      // If no rows were changed, then the ID must not exist, so 404
       return res.status(404).end();
     } else {
       res.status(200).end();
@@ -44,5 +38,5 @@ router.put("/api/burgers/:id", function(req, res) {
   });
 });
 
-// Export routes for server.js to use.
 module.exports = router;
+
--- server.js
+++ server.js
@@ -1,15 +1,24 @@
 var express = require("express");
 var bodyParser = require("body-parser");
-var port = process.env.PORT || 3000;
+var PORT = process.env.PORT || 3000;
 var app = express();
-// Serve static content for the app from the "public" directory in the application directory.
+var db = require("./models");
 app.use(express.static("public"));
 app.use(bodyParser.urlencoded({ extended: false }));
-// Set Handlebars.
 var exphbs = require("express-handlebars");
 app.engine("handlebars", exphbs({ defaultLayout: "main" }));
 app.set("view engine", "handlebars");
-// Import routes and give the server access to them.
-var routes = require("./controllers/burgers_controller.js");
-app.use("/", routes);
-app.listen(port);
+var router = require("./controllers/burgers_controller.js");
+
+app.use("/", router);
+
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.text());
+app.use(bodyParser.json({ type: "application/vnd.api+json" }));
+app.use(express.static("public"));
+
+db.sequelize.sync().then(function() {
+  app.listen(PORT, function() {
+    console.log("App listening on PORT " + PORT);
+  });
+ });
